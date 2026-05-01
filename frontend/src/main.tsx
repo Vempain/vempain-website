c692cbd9be06a2153e7ec1fdeb3afe89f34970ed
Paul-Erik Törrönen <poltsi@poltsi.fi>
2026-05-01 19:22:35 +0300

    [frontend] Fix missing location marker in build
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import L from 'leaflet'
 import './index.css'
 import 'leaflet/dist/leaflet.css';
+import markerIcon2xUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIconUrl from 'leaflet/dist/images/marker-icon.png'
+import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png'
 import App from './App.tsx'
 import {AuthProvider, ThemeProvider} from './context'
+
+// Ensure Leaflet marker assets are bundled and resolved with absolute built URLs.
+delete (L.Icon.Default.prototype as { _getIconUrl?: unknown })._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2xUrl,
+    iconUrl: markerIconUrl,
+    shadowUrl: markerShadowUrl,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
